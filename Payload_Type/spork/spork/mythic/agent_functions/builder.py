6a16836870caae3fc24952bf3b72440ec0b2bde9
smokiieee <57633388+adityabaronia@users.noreply.github.com>
2024-11-05 18:11:30 +0530

    Update builder.py

--- Payload_Type/spork/spork/mythic/agent_functions/builder.py
+++ Payload_Type/spork/spork/mythic/agent_functions/builder.py
@@ -82,7 +82,7 @@ class spork(PayloadType):
             os.system(f"echo '{key}' : '{value}' >> c2info.txt")
 
         # finding all parameter
-        parameter = self.get_parameter()
+        parameter = self.get_parameter[0]
         parameter_dict = parameter.get_parameter_dict()
         for key, value in parameter_dict.items():
             os.system(f"echo '{key}' : '{value}' >> get_parameter.txt")
